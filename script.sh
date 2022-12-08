97fd152ce0926fde937fb49ea7c48e5801327ff9
hunter-gunter <32549091+hunter-gunter@users.noreply.github.com>
2022-12-08 10:24:40 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 apt update
-apt install openjdk-11-jdk
+apt install openjdk-11-jdk -y
 curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/ | tee /etc/apt/sources.list.d/jenkins.list > /dev/null
-apt-get update
-apt-get install jenkins
+apt update
+apt-get install jenkins -y
 service jenkins start
